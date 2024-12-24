7c67115bbfd469514eafca15632daf6dd24b9f78
zemccartney <zemccartney@gmail.com>
2024-12-24 13:50:04 -0500

    biome test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+	integrations: [react(), tailwind()],
 });
--- eslint.config.js
+++
@@ -1,7 +0,0 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs['jsx-a11y-strict']
-];
