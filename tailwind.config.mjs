a1f5a1b063a58720d085750718afd99266b09c2c
zemccartney <zemccartney@gmail.com>
2024-12-24 13:26:57 -0500

    bootstrap

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-emerald": "#032203",
+      },
+      fontFamily: {
+        sans: ["ChicagoKare-Regular", ...defaultTheme.fontFamily.sans],
+        mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
+        title: ["Sixtyfour Variable", ...defaultTheme.fontFamily.mono]
+      },
+    },
+  },
+  plugins: [],
+};
