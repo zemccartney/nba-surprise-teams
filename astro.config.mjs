d074cd5f408338f9d3eb917ca0525effad9ed573
zemccartney <zemccartney@gmail.com>
2025-01-01 18:21:36 -0500

    Fix vite build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,4 +24,15 @@ export default defineConfig({
       }),
     },
   },
+  vite: {
+    resolve: {
+      // https://github.com/withastro/adapters/pull/436#issuecomment-2525190557
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      // @ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
