1c2f6c377867e7f49b247ac15a0f6517ae74f371
zemccartney <zemccartney@gmail.com>
2025-01-04 17:52:18 -0500

    Try inlining again because why not

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -184,6 +184,25 @@ export const server = {
           // console.log(err);
         }
 
+        Sentry.init({
+          dsn: import.meta.env.PUBLIC_SENTRY_DSN,
+          environment: import.meta.env.PUBLIC_DEPLOY_ENV,
+
+          // Define how likely traces are sampled. Adjust this value in production,
+          // or use tracesSampler for greater control.
+          tracesSampleRate: 0.1,
+
+          // This sets the sample rate to be 10%. You may want this to be 100% while
+          // in development and sample at a lower rate in production
+          replaysSessionSampleRate: 0.1,
+
+          // If the entire session is not sampled, use the below sample rate to sample
+          // sessions when an error occurs.
+          replaysOnErrorSampleRate: 0.1,
+
+          debug: true,
+        });
+
         // @ts-ignore
         console.log(ctx.locals.__sentry_wrapped__);
 
