79c68e2b5eb20af12b58ce170bc3127172bcdb3d
zemccartney <zemccartney@gmail.com>
2025-01-04 15:04:59 -0500

    inline sentry init

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -183,6 +183,24 @@ export const server = {
         if (import.meta.env.DEV) {
           // console.log(err);
         }
+
+        // Very not good but I just need something to work please
+        Sentry.init({
+          dsn: import.meta.env.PUBLIC_SENTRY_DSN,
+          environment: import.meta.env.PUBLIC_DEPLOY_ENV,
+          // Define how likely traces are sampled. Adjust this value in production,
+          // or use tracesSampler for greater control.
+          tracesSampleRate: 0.1,
+
+          // This sets the sample rate to be 10%. You may want this to be 100% while
+          // in development and sample at a lower rate in production
+          replaysSessionSampleRate: 0.1,
+
+          // If the entire session is not sampled, use the below sample rate to sample
+          // sessions when an error occurs.
+          replaysOnErrorSampleRate: 0.1,
+        });
+
         Sentry.captureException(err);
         throw err;
       }
