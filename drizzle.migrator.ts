ae1d640cdc5b612feec394eee03f3ea27f855aa5
zemccartney <zemccartney@gmail.com>
2025-01-07 20:01:24 -0500

    Fix migrator config passing

--- drizzle.migrator.ts
+++ drizzle.migrator.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/libsql/web";
 import { migrate } from "drizzle-orm/libsql/migrator";
 
 import Config from "./drizzle.config";
@@ -7,7 +7,12 @@ import Config from "./drizzle.config";
 // Script needed, as assumed drizzle-kit won't be installed in production environments due to usage of
 // npm clean-install by CF (no dev deps if NODE_ENV=production)
 
-const db = drizzle(process.env.TURSO_URL!);
+const db = drizzle({
+  connection: {
+    url: process.env.TURSO_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
 await migrate(db, {
   migrationsFolder: Config.out,
 });
