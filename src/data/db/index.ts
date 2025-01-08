39cb04d234182b1f0c9715adae3463b1945d508b
zemccartney <zemccartney@gmail.com>
2025-01-07 21:40:29 -0500

    log db client misconfig

--- src/data/db/index.ts
+++ src/data/db/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client/web";
 
+console.log(import.meta.env, process?.env, "WHAT THE HELL IS GOING ON HERE");
+
 const turso = createClient({
   url: import.meta.env.TURSO_URL!,
   authToken: import.meta.env.TURSO_AUTH_TOKEN,
