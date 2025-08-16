b7299d64438075c8d239731865aa3c1627376511
Zack McCartney <zemccartney@gmail.com>
2025-08-16 18:29:31 -0400

    Reinstate vitest lowered teardown timeout, apparently still affected by cloudflare runtime hanging, need to investigate further when I find the time

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
+    // Works around astro bug, failing to shut down cloudflare's runtime,
+    // resulting in a hanging process error
+    teardownTimeout: 1000,
     watchTriggerPatterns: [
       // rebuild on changes to astro source, since test files don't import directly (and vitest watch watches by import graph) (really, only care about content, consider narrowing)
       {
